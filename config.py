ed90d97c5453ff0a78f49566557625a8b8694e01
Axial - Security <axial.security@gmail.com>
2026-06-15 21:08:19 -0300

    Version N°9 Paso 2

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # --- Identidad / version ---
-    APP_VERSION = '0.8.0'
+    APP_VERSION = '0.9.0'
     APP_NOMBRE = 'Saludables'
     APP_SUBTITULO = 'Catalogo Mayorista · Pilar'
 
